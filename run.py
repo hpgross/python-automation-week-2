cc9a2735d2439608452bf0151f6bf509e8eb03ed
hpgross <hpgross@gmail.com>
2021-05-16 18:31:33 -0400

    Removed the cli input as the problem didn't need it
    Fixed the problem with the environment delimiter
    
    Added a section for where the IP address can be inserted
    
    Fixed the order on the files for the dictionary
    
    Changed the variable name from post to new_post to avoid potential
    keyword collisions

--- run.py
+++ run.py
@@ -1,20 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import sys, os, json, requests
+import os
+import requests
 
-server_location = "http://" + sys.argv[1] + "/feedback/"
-file_list = os.listdir("/data/requests")
+ip_address = ""
+server_location = "http://" + ip_address + "/feedback/"
+file_list = os.listdir("/data/feedback")
 current_request = {}
 
 for file in file_list:
     #set up the dictionary for the request
-    open_file = open(file,r)
+    open_file = open("/data/feedback/"+file,"r")
     line_list = open_file.read().splitlines()
     current_request["title"] = line_list[0]
-    current_request["date"] = line_list[1]
-    current_request["name"] = line_list[2]
+    current_request["name"] = line_list[1]
+    current_request["date"] = line_list[2]
     current_request["feedback"] = line_list[3]
     open_file.close()
 
-    post = requests.post(server_location,json=current_request)
-    post.raise_for_status()
+    new_post = requests.post(server_location,json=current_request)
+    new_post.raise_for_status()
+    print(new_post.status_code)
